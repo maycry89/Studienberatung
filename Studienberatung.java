026dc67c43caf14c1797e7701691c167debd8668
HAWMAILCLUSTER\abv263 <maycry@gmx.de>
2016-10-12 16:21:35 +0200

    added question4i

--- Studienberatung.java
+++ Studienberatung.java
@@ -7,13 +7,18 @@ public class Studienberatung{
 			System.out.println("Hast du Abitur?");
 			String answer1 = scan.next();
 
-			if(answert.equalsIgnoreCase("n") {
+			if(answer1.equalsIgnoreCase("n") {
 				System.out.println("Hast du Fachbitur?");
 				String answer2 = scan.next();
+				
+				if(answer2.equalsIgnoreCase("j"){
+					System.out.println(Hast du eine Berufsausbildung?);
+					String answer4 = scan.next();
+				}
 			}
 			
 
-			else if (anser.equalsIgnoreCase("j"){
+			else if (answer1.equalsIgnoreCase("j"){
 				System.out.println("Bist du wissbegierig?"
 				String answer3 = scan.next();
 			}
